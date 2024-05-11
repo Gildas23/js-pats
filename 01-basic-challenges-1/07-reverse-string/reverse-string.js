eb116e42565dd83ebde6cca1c33507ac9588ad72
Chatue Sobgoui Gildas <gildassob@gmail.com>
2024-05-11 09:27:12 +0100

    finished reverse strings

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -2,9 +2,9 @@ function reverseString(str) {
     let rev = '';
 
     for (let i=str.length-1; i>= 0; i--){
-        console.log(" damn : ",str[i])
 
         rev += str[i];
+    
     }
     
     // console.log(`reverse : ${revStr}\nnormal : ${str}`)
